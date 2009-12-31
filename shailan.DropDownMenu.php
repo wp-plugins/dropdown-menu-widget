e331b68d3f96d60a04aab383926f779a27c2a945
mattsay <mattsay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-31 09:24:18 +0000

    Added translations.
    git-svn-id: https://plugins.svn.wordpress.org/dropdown-menu-widget/trunk@188376 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shailan.DropDownMenu.php
+++ shailan.DropDownMenu.php
@@ -6,6 +6,7 @@ Description: A multi widget to generate drop-down menus from your pages and cate
 Version: 1.2.4
 Author: Matt Say
 Author URI: http://shailan.com
+Text Domain: shailan-dropdown-menu
 */
 
 define('SHAILAN_DM_VERSION','1.2.4');
@@ -18,7 +19,7 @@ define('SHAILAN_DM_FOLDER', 'dropdown-menu-widget');
 class shailan_DropdownWidget extends WP_Widget {
     /** constructor */
     function shailan_DropdownWidget() {
-		$widget_ops = array('classname' => 'shailan-dropdown-menu', 'description' => __( 'Dropdown page/category menu' ) );
+		$widget_ops = array('classname' => 'shailan-dropdown-menu', 'description' => __( 'Dropdown page/category menu', 'shailan-dropdown-menu' ) );
 		$this->WP_Widget('dropdown-menu', __('Dropdown Menu'), $widget_ops);
 		$this->alt_option_name = 'widget_dropdown_menu';
 		
@@ -29,7 +30,7 @@ class shailan_DropdownWidget extends WP_Widget {
 	// Add settings page
 	function adminMenu(){
 		if (function_exists('add_options_page')) {
-			add_options_page('Settings for Dropdown Menu by Shailan' , 'Dropdown Menu', 9, 'dropdown-menu', array('shailan_DropdownWidget', 'getOptionsPage'));
+			add_options_page(__('Settings for Dropdown Menu', 'shailan-dropdown-menu') , __('Dropdown Menu', 'shailan-dropdown-menu'), 9, 'dropdown-menu', array('shailan_DropdownWidget', 'getOptionsPage'));
 		}
 	}
 	
@@ -78,7 +79,7 @@ class shailan_DropdownWidget extends WP_Widget {
 		update_option($width_tag, $width);
 		
 		?>
-		<div class="updated"><p><strong><?php _e('Options saved.', 'shailanDropdownMenu_domain'); ?></strong></p></div>
+		<div class="updated"><p><strong><?php _e('Options saved.', 'shailan-dropdown-menu'); ?></strong></p></div>
 		
 		<?php
 	}
@@ -112,7 +113,7 @@ Please support if you like this plugin:
 </form>
 </div>
 
-<p>Dropdown menu creates a beautiful CSS only dropdown menu from your wordpress pages or categories. You can customize dropdown menu theme and settings here: </p>
+<p><?php _e('Dropdown menu creates a beautiful CSS only dropdown menu from your wordpress pages or categories. You can customize dropdown menu theme and settings here:', 'shailan-dropdown-menu'); ?></p>
 
 <form id="frmShailanDm" name="frmShailanDm" method="post" action="">
 
@@ -120,7 +121,7 @@ Please support if you like this plugin:
 
 <table class="form-table">
 <tr valign="top">
-<th scope="row"><label for="<?php echo $theme_tag; ?>"><?php _e('Dropdown menu theme') ?></label></th>
+<th scope="row"><label for="<?php echo $theme_tag; ?>"><?php _e('Dropdown menu theme', 'shailan-dropdown-menu') ?></label></th>
 <td><select name="<?php echo $theme_tag; ?>" id="theme">
 
 <?php foreach($themes as $name=>$path){
@@ -128,46 +129,46 @@ Please support if you like this plugin:
 				echo '<option value="'.$path.'" '.$selected.'>'.$name.'</option>';
 			} ?>
 
-</select> <span class="description"><?php _e('You can choose a theme for your dropdown menu here.') ?></span></td>
+</select> <span class="description"><?php _e('You can choose a theme for your dropdown menu here.', 'shailan-dropdown-menu'); ?></span></td>
 </tr>
 </table>
 
 <fieldset width="400">
-<h2>Template tag options</h2>
-<p>You can use following template tag in your themes to display the dropdown menu. <br />
+<h2><?php _e('Template tag options', 'shailan-dropdown-menu'); ?></h2>
+<p><?php _e('You can use following template tag in your themes to display the dropdown menu.', 'shailan-dropdown-menu'); ?><br />
 <blockquote><code>&lt;?php if(function_exists('shailan_dropdown_menu')){ shailan_dropdown_menu(); } ?&gt;</code></blockquote> 
-Here you can set template tag options: 
+<?php _e('Here you can set template tag options:', 'shailan-dropdown-menu'); ?>
 </p>
 <div style="padding:10px; border:1px solid #ddd; width:275px; ">
 <!-- <p><label for="<?php echo $title_tag; ?>"><?php _e('Title (won\'t be shown):'); ?> <input class="widefat" id="<?php echo $title_tag; ?>" name="<?php echo $title_tag; ?>" type="text" value="<?php echo $title; ?>" /></label></p> -->
 			
-		<p><?php _e('Type:'); ?> <label for="Pages"><input type="radio" id="Pages" name="<?php echo $type_tag; ?>" value="Pages" <?php if($type=='Pages'){ echo 'checked="checked"'; } ?> /> <?php _e('Pages'); ?></label> <label for="Categories"><input type="radio" id="Categories" name="<?php echo $type_tag; ?>" value="Categories" <?php if($type=='Categories'){ echo 'checked="checked"'; } ?>/> <?php _e('Categories'); ?></label></p>
+		<p><?php _e('Type:'); ?> <label for="Pages"><input type="radio" id="Pages" name="<?php echo $type_tag; ?>" value="Pages" <?php if($type=='Pages'){ echo 'checked="checked"'; } ?> /> <?php _e('Pages', 'shailan-dropdown-menu'); ?></label> <label for="Categories"><input type="radio" id="Categories" name="<?php echo $type_tag; ?>" value="Categories" <?php if($type=='Categories'){ echo 'checked="checked"'; } ?>/> <?php _e('Categories', 'shailan-dropdown-menu'); ?></label></p>
 			
-		<p><label for="<?php echo $exclude_tag; ?>"><?php _e('Exclude:'); ?> <input class="widefat" id="<?php echo $exclude_tag; ?>" name="<?php echo $exclude_tag; ?>" type="text" value="<?php echo $exclude; ?>" /></label><br /> 
-		<small>Page IDs, separated by commas.</small></p>
+		<p><label for="<?php echo $exclude_tag; ?>"><?php _e('Exclude:', 'shailan-dropdown-menu'); ?> <input class="widefat" id="<?php echo $exclude_tag; ?>" name="<?php echo $exclude_tag; ?>" type="text" value="<?php echo $exclude; ?>" /></label><br /> 
+		<small><?php _e('Page IDs, separated by commas.', 'shailan-dropdown-menu'); ?></small></p>
 			
 		<p>
 		<input type="checkbox" class="checkbox" id="<?php echo $login_tag; ?>" name="<?php echo $login_tag; ?>"<?php checked( $login ); ?> />
-		<label for="<?php echo $login_tag; ?>"><?php _e( 'Add login/logout' ); ?></label><br />
+		<label for="<?php echo $login_tag; ?>"><?php _e( 'Add login/logout', 'shailan-dropdown-menu' ); ?></label><br />
 		<input type="checkbox" class="checkbox" id="<?php echo $admin_tag; ?>" name="<?php echo $admin_tag; ?>"<?php checked( $admin ); ?> />
-		<label for="<?php echo $admin_tag; ?>"><?php _e( 'Add Register/Site Admin' ); ?></label><br />
+		<label for="<?php echo $admin_tag; ?>"><?php _e( 'Add Register/Site Admin', 'shailan-dropdown-menu' ); ?></label><br />
 		<input type="checkbox" class="checkbox" id="<?php echo $vertical_tag; ?>" name="<?php echo $vertical_tag; ?>"<?php checked( $vertical ); ?> />
-		<label for="<?php echo $vertical_tag; ?>"><?php _e( 'Vertical menu' ); ?></label>
+		<label for="<?php echo $vertical_tag; ?>"><?php _e( 'Vertical menu', 'shailan-dropdown-menu' ); ?></label>
 		</p>
 		
-		<p><label for="<?php echo $inline_style_tag; ?>"><?php _e('Inline Style:'); ?> <input class="widefat" id="<?php echo $inline_style_tag; ?>" name="<?php echo $inline_style_tag; ?>" type="text" value="<?php echo $inline_style; ?>" /></label><br /> 
-			<small>Applied to menu container &lt;div&gt;.</small></p>
+		<p><label for="<?php echo $inline_style_tag; ?>"><?php _e('Inline Style:', 'shailan-dropdown-menu'); ?> <input class="widefat" id="<?php echo $inline_style_tag; ?>" name="<?php echo $inline_style_tag; ?>" type="text" value="<?php echo $inline_style; ?>" /></label><br /> 
+			<small><?php _e('Applied to menu container &lt;div&gt;', 'shailan-dropdown-menu'); ?></small></p>
 			
-<p><small>NOTE: Widgets have their own options. Those options won't affect widgets.</small></p>
+<p><small><?php _e('NOTE: Widgets have their own options. Those options won\'t affect widgets.', 'shailan-dropdown-menu'); ?></small></p>
 </div>
 </fieldset>
 
-	<p>NOTE : Onscreen theme edit will be available soon. Be sure to check <a href="http://shailan.com">shailan.com</a> regularly for updates.</p>
+	<p><?php _e('NOTE : Onscreen theme edit will be available soon. Be sure to check <a href="http://shailan.com">shailan.com</a> regularly for updates.', 'shailan-dropdown-menu'); ?></p>
 </div>
 <p class="submit">
-<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes', 'shailan-dropdown-menu'); ?>" />
 </p>
-<p>Visit <a href="http://shailan.com">shailan.com</a> for more wordpress themes, plugins and widgets.</p>
+<p><?php _e('Visit <a href="http://shailan.com">shailan.com</a> for more wordpress themes, plugins and widgets.', 'shailan-dropdown-menu'); ?></p>
 </form>
 <p>
 <a href="http://shailan.com/wordpress/plugins/dropdown-menu">Dropdown Menu <?php echo SHAILAN_DM_VERSION; ?></a> by <a href="http://shailan.com">shailan</a></a> &copy; 2009 
@@ -218,7 +219,7 @@ Here you can set template tag options:
 					<tr><td> 
 					<ul class="dropdown <?php echo $orientation; ?>">
 					<?php if($type == 'Pages'){ ?>
-						<li class="<?php if ( is_front_page() && !is_paged() ): ?>current_page_item<?php else: ?>page_item<?php endif; ?> blogtab"><a href="<?php echo get_option('home'); ?>/"><span><?php _e('Home'); ?></span></a></li>	
+						<li class="<?php if ( is_front_page() && !is_paged() ): ?>current_page_item<?php else: ?>page_item<?php endif; ?> blogtab"><a href="<?php echo get_option('home'); ?>/"><span><?php _e('Home', 'shailan-dropdown-menu'); ?></span></a></li>	
 						<?php 
 						$page_walker = new shailan_PageWalker();
 						wp_list_pages(array(
@@ -265,29 +266,29 @@ Here you can set template tag options:
 		$align = $instance['align'];
 		
         ?>		
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title (won\'t be shown):'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title (won\'t be shown):', 'shailan-dropdown-menu'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
 			
-		<p><?php _e('Type:'); ?> <label for="Pages"><input type="radio" id="Pages" name="<?php echo $this->get_field_name('type'); ?>" value="Pages" <?php if($type=='Pages'){ echo 'checked="checked"'; } ?> /> <?php _e('Pages'); ?></label> <label for="Categories"><input type="radio" id="Categories" name="<?php echo $this->get_field_name('type'); ?>" value="Categories" <?php if($type=='Categories'){ echo 'checked="checked"'; } ?>/> <?php _e('Categories'); ?></label></p>
+		<p><?php _e('Type:'); ?> <label for="Pages"><input type="radio" id="Pages" name="<?php echo $this->get_field_name('type'); ?>" value="Pages" <?php if($type=='Pages'){ echo 'checked="checked"'; } ?> /> <?php _e('Pages', 'shailan-dropdown-menu'); ?></label> <label for="Categories"><input type="radio" id="Categories" name="<?php echo $this->get_field_name('type'); ?>" value="Categories" <?php if($type=='Categories'){ echo 'checked="checked"'; } ?>/> <?php _e('Categories', 'shailan-dropdown-menu'); ?></label></p>
 			
-		<p><label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e('Exclude:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('exclude'); ?>" name="<?php echo $this->get_field_name('exclude'); ?>" type="text" value="<?php echo $exclude; ?>" /></label><br /> 
+		<p><label for="<?php echo $this->get_field_id('exclude'); ?>"><?php _e('Exclude:', 'shailan-dropdown-menu'); ?> <input class="widefat" id="<?php echo $this->get_field_id('exclude'); ?>" name="<?php echo $this->get_field_name('exclude'); ?>" type="text" value="<?php echo $exclude; ?>" /></label><br /> 
 		<small>Page IDs, separated by commas.</small></p>
 			
 		<p>
 		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('login'); ?>" name="<?php echo $this->get_field_name('login'); ?>"<?php checked( $login ); ?> />
-		<label for="<?php echo $this->get_field_id('login'); ?>"><?php _e( 'Add login/logout' ); ?></label><br />
+		<label for="<?php echo $this->get_field_id('login'); ?>"><?php _e( 'Add login/logout' , 'shailan-dropdown-menu' ); ?></label><br />
 		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('admin'); ?>" name="<?php echo $this->get_field_name('admin'); ?>"<?php checked( $admin ); ?> />
-		<label for="<?php echo $this->get_field_id('admin'); ?>"><?php _e( 'Add Register/Site Admin' ); ?></label><br />
+		<label for="<?php echo $this->get_field_id('admin'); ?>"><?php _e( 'Add Register/Site Admin' , 'shailan-dropdown-menu' ); ?></label><br />
 		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('vertical'); ?>" name="<?php echo $this->get_field_name('vertical'); ?>"<?php checked( $vertical ); ?> />
-		<label for="<?php echo $this->get_field_id('vertical'); ?>"><?php _e( 'Vertical menu ' ); ?></label>
+		<label for="<?php echo $this->get_field_id('vertical'); ?>"><?php _e( 'Vertical menu' , 'shailan-dropdown-menu' ); ?></label>
 		</p>
 		
-		<p><?php _e('Align:'); ?> <label for="left"><input type="radio" id="left" name="<?php echo $this->get_field_name('align'); ?>" value="left" <?php if($align=='left'){ echo 'checked="checked"'; } ?> /> <?php _e('Left'); ?></label> <label for="center"><input type="radio" id="center" name="<?php echo $this->get_field_name('align'); ?>" value="center" <?php if($align=='center'){ echo 'checked="checked"'; } ?>/> <?php _e('Center'); ?></label> <label for="right"><input type="radio" id="right" name="<?php echo $this->get_field_name('align'); ?>" value="right" <?php if($align=='right'){ echo 'checked="checked"'; } ?>/> <?php _e('Right'); ?></label></p>
+		<p><?php _e('Align:', 'shailan-dropdown-menu'); ?> <label for="left"><input type="radio" id="left" name="<?php echo $this->get_field_name('align'); ?>" value="left" <?php if($align=='left'){ echo 'checked="checked"'; } ?> /> <?php _e('Left', 'shailan-dropdown-menu'); ?></label> <label for="center"><input type="radio" id="center" name="<?php echo $this->get_field_name('align'); ?>" value="center" <?php if($align=='center'){ echo 'checked="checked"'; } ?>/> <?php _e('Center', 'shailan-dropdown-menu'); ?></label> <label for="right"><input type="radio" id="right" name="<?php echo $this->get_field_name('align'); ?>" value="right" <?php if($align=='right'){ echo 'checked="checked"'; } ?>/> <?php _e('Right', 'shailan-dropdown-menu'); ?></label></p>
 		
-		<p><label for="<?php echo $this->get_field_id('style'); ?>"><?php _e('Inline Style:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>" type="text" value="<?php echo $inline_style; ?>" /></label><br /> 
-			<small>Applied to menu container &lt;div&gt;.</small></p>
+		<p><label for="<?php echo $this->get_field_id('style'); ?>"><?php _e('Inline Style:', 'shailan-dropdown-menu'); ?> <input class="widefat" id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>" type="text" value="<?php echo $inline_style; ?>" /></label><br /> 
+			<small><?php _e('Applied to menu container &lt;div&gt;.', 'shailan-dropdown-menu'); ?></small></p>
 			
 <div class="widget-control-actions alignright">
-<p><small><a href="options-general.php?page=dropdown-menu">Menu Style</a> | <a href="http://shailan.com/wordpress/plugins/dropdown-menu">Visit plugin site</a></small></p>
+<p><small><a href="options-general.php?page=dropdown-menu"><?php esc_attr_e('Menu Style', 'shailan-dropdown-menu'); ?></a> | <a href="http://shailan.com/wordpress/plugins/dropdown-menu"><?php esc_attr_e('Visit plugin site', 'shailan-dropdown-menu'); ?></a></small></p>
 </div>
 			
         <?php 
@@ -315,11 +316,18 @@ Here you can set template tag options:
 
 // register widget
 add_action('widgets_init', create_function('', 'return register_widget("shailan_DropdownWidget");'));
+
+// load translations
+$plugin_dir = basename(dirname(__FILE__));
+load_plugin_textdomain( 'shailan-dropdown-menu', 'wp-content/plugins/' . $plugin_dir, $plugin_dir );
+
+// add admin menu
 add_action('admin_menu', array('shailan_DropdownWidget', 'adminMenu'));
 
 include('shailan-page-walker.php'); // Load custom page walker
 include('shailan-category-walker.php'); // Load custom category walker
 
+// template tag support
 function shailan_dropdown_menu(){
 	$type = get_option('shailan_dm_type');
 	$exclude = get_option('shailan_dm_exclude');
